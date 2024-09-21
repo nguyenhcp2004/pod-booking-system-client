0baa7ddc7497c575bb6df7016077432bc54f0127
Quoc Huy <huy.nguyen@omnibox.com.vn>
2024-09-21 14:42:21 +0700

    [HuyNBQ] feat: config mui theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ThemeProvider } from '@mui/material'
+import { theme } from './themes/theme'
 
 function App() {
   return (
     <>
-      <div>nguyenhuynh</div>
-      <Button variant='contained'>Hello world</Button>
-      <AccessAlarmIcon />
-      <ReactQueryDevtools initialIsOpen={false} />
+      <ThemeProvider theme={theme}>
+        <div>nguyenhuynh</div>
+        <Button variant='contained'>Hello world</Button>
+        <AccessAlarmIcon />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ThemeProvider>
     </>
   )
 }
