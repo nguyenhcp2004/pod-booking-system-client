296bc32aa0d8bfc8b5a72b4ccca567950ff63344
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-09-26 11:23:43 +0700

    [PBS-42][HoangHN] feat:🖕title of rooms section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,21 +5,41 @@ import homePageBanner from '../../assets/images/homePageBanner.png'
 
 export default function Home() {
   return (
-    <Box sx={{ width: '100%', paddingX: '104px', paddingY: '24px' }}>
-      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        <Grid item xs={6}>
-          <Typography variant='h3' sx={{ fontWeight: 'bold' }} color='primary'>
+    <Box sx={{ width: '100%' }}>
+      {/* Hero Secion */}
+      <Grid
+        container
+        rowSpacing={1}
+        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+        sx={{ paddingX: '104px', paddingY: '24px' }}
+      >
+        <Grid item xs={6} style={{ alignContent: 'center' }}>
+          <Typography variant='h2' sx={{ fontWeight: 'bold' }} color='primary'>
             FlexiPod
           </Typography>
-          <Typography variant='h3' color='secondary'>
+          <Typography variant='h2' color='secondary'>
             Tiện lợi, riêng tư
           </Typography>
-          <Typography variant='subtitle2' color='neutral'>
+          <Typography variant='subtitle1' color='neutral'>
             Không gian tích hợp đa dạng dịch vụ giúp thúc đẩy công việc của bạn phát triển một cách tối đa.
           </Typography>
         </Grid>
         <Grid item xs={6}>
-          <img src={homePageBanner} alt='' />
+          <img src={homePageBanner} alt='' style={{ borderRadius: '8px', width: '100%' }} />
+        </Grid>
+      </Grid>
+
+      {/* Rooms Section  */}
+      <Grid
+        container
+        rowSpacing={1}
+        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+        sx={{ paddingX: '104px', paddingY: '24px' }}
+      >
+        <Grid item xs={12}>
+          <Typography variant='h2' sx={{ fontWeight: 'bold', textAlign: 'center' }} color='primary'>
+            Đặt phòng
+          </Typography>
         </Grid>
       </Grid>
     </Box>
