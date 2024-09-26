4bcf170e096eabbd52d842fbdd9f0c89ebd8cdb3
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-09-26 11:51:29 +0700

    [PBS-42][HoangHN]:🖕 filter room ui

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
-import { Typography } from '@mui/material'
+import { FormControl, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material'
 import homePageBanner from '../../assets/images/homePageBanner.png'
 
 export default function Home() {
@@ -40,6 +40,63 @@ export default function Home() {
           <Typography variant='h2' sx={{ fontWeight: 'bold', textAlign: 'center' }} color='primary'>
             Đặt phòng
           </Typography>
+        </Grid>
+
+        <Grid container spacing={2} style={{ paddingTop: '32px', paddingBottom: '32px' }}>
+          <Grid item xs={3}>
+            <FormControl fullWidth>
+              <InputLabel id='location-label'>Địa chỉ</InputLabel>
+              <Select
+                labelId='location-label'
+                // value={location}
+                label='Địa chỉ'
+                // onChange={(e) => setLocation(e.target.value)}
+              >
+                <MenuItem value='TP.Thủ Đức'>TP.Thủ Đức</MenuItem>
+                {/* Add more locations as needed */}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={3}>
+            <FormControl fullWidth>
+              <InputLabel id='room-type-label'>Loại phòng</InputLabel>
+              <Select
+                labelId='room-type-label'
+                // value={roomType}
+                label='Loại phòng'
+                // onChange={(e) => setRoomType(e.target.value)}
+              >
+                <MenuItem value='Phòng 2 người'>Phòng 2 người</MenuItem>
+                {/* Add more room types as needed */}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={3}>
+            <TextField
+              fullWidth
+              label='Ngày đặt'
+              type='date'
+              // value={date}
+              // onChange={(e) => setDate(e.target.value)}
+              InputLabelProps={{
+                shrink: true
+              }}
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <FormControl fullWidth>
+              <InputLabel id='time-slot-label'>Slot</InputLabel>
+              <Select
+                labelId='time-slot-label'
+                // value={timeSlot}
+                label='Slot'
+                // onChange={(e) => setTimeSlot(e.target.value)}
+              >
+                <MenuItem value='7h - 9h'>7h - 9h</MenuItem>
+                {/* Add more time slots as needed */}
+              </Select>
+            </FormControl>
+          </Grid>
         </Grid>
       </Grid>
     </Box>
