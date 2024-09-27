31879046df873a23cff5d3d92d62195cf3cdb395
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-09-27 11:06:22 +0700

    [PBS-42][HoangHN] fix: 💀 fix merge error

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,18 @@ import 'react-toastify/dist/ReactToastify.css'
 import RefreshToken from '~/components/RefreshToken'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider/LocalizationProvider'
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
-import 'moment/locale/en-gb';
+import 'moment/locale/en-gb'
 function App() {
   const routeElements = useRouteElements()
   return (
     <>
-      <ThemeProvider theme={theme}>
-        {routeElements}
-        <RefreshToken />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </ThemeProvider>
-      <ToastContainer />
       <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale='en-gb'>
         <ThemeProvider theme={theme}>
           {routeElements}
+          <RefreshToken />
           <ReactQueryDevtools initialIsOpen={false} />
         </ThemeProvider>
+        <ToastContainer />
       </LocalizationProvider>
     </>
   )
