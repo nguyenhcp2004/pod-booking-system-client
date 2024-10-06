d83654b88fa4bd133ee474dd7f6281e8cc135379
Huynh Nguyen <yugivip277@gmail.com>
2024-10-06 11:43:18 +0700

    [PBS-64][NguyenHCP] feat: code NavItem side bar

--- src/components/AdminSidebar/AdminSideBar.tsx
+++ src/components/AdminSidebar/AdminSideBar.tsx
@@ -1,9 +1,45 @@
+import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
+import NavItem from '~/components/AdminSidebar/NavItem'
 
 export default function AdminSideBar() {
   return (
-    <Box sx={{ padding: '20px 20px 0px', height: '100vh', width: '300px', borderRight: '1px solid #DFE3E8' }}>
-      AdminSideBar
+    <Box
+      sx={{
+        padding: '20px 20px 0px',
+        height: '100vh',
+        width: '300px',
+        borderRight: '1px solid #DFE3E8',
+        display: 'flex',
+        flexDirection: 'column'
+      }}
+    >
+      <Typography
+        variant='h3'
+        component={'a'}
+        href='/admin'
+        sx={{
+          fontWeight: '700',
+          lineHeight: '120%',
+          color: 'primary.main',
+          textDecoration: 'none',
+          paddingLeft: '12px'
+        }}
+      >
+        Flexipod
+      </Typography>
+      <Box
+        sx={{
+          marginY: '16px',
+          padding: '24px 12px 24px 16px',
+          flexGrow: 1,
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 0.5
+        }}
+      >
+        <NavItem />
+      </Box>
     </Box>
   )
 }
