f2924f1541887fc9fcbca38c6ec5aac056069e56
toki-ai <lucngan2004@gmail.com>
2024-10-16 04:09:59 +0700

    [PBS-83][Anh Dao] fix: fix table component in room type

--- src/pages/ManageRoomType/ManageRoomType.tsx
+++ src/pages/ManageRoomType/ManageRoomType.tsx
@@ -1,219 +1,96 @@
-import { Box, Button, MenuItem, Select, Typography } from '@mui/material'
-import {
-  GridActionsCellItem,
-  GridColDef,
-  GridRenderCellParams,
-  GridRowId,
-  GridRowModes,
-  GridRowModesModel,
-  GridToolbarContainer,
-  GridValidRowModel
-} from '@mui/x-data-grid'
-import SaveIcon from '@mui/icons-material/Save'
-import CancelIcon from '@mui/icons-material/Cancel'
-import EditIcon from '@mui/icons-material/Edit'
-import DeleteIcon from '@mui/icons-material/Delete'
-import AddIcon from '@mui/icons-material/Add'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { Box, Typography, CircularProgress } from '@mui/material'
+import { GridColDef, GridToolbarContainer, GridToolbarQuickFilter } from '@mui/x-data-grid'
+import { useGetFilterRoomType } from '~/queries/useFilterRoomType'
+import RoomTypeModal from './RoomTypeModal'
+import { ACTION } from '~/constants/mock'
 import Table from '~/components/Table/Table'
 
-const buildings = ['Tòa nhà A', 'Tòa nhà B', 'Tòa nhà C']
-
-const initialRows: GridValidRowModel[] = [
-  {
-    id: 1,
-    name: 'Phòng 2 người',
-    price: 500000,
-    quantity: 10,
-    capacity: 2,
-    rooms: ['101', '102', '103', '104', '105'],
-    building: 'Tòa nhà A'
-  },
-  {
-    id: 2,
-    name: 'Phòng 3 người',
-    price: 750000,
-    quantity: 5,
-    capacity: 3,
-    rooms: ['201', '202', '203'],
-    building: 'Tòa nhà B'
-  },
-  {
-    id: 3,
-    name: 'Phòng VIP',
-    price: 1000000,
-    quantity: 2,
-    capacity: 4,
-    rooms: ['301'],
-    building: 'Tòa nhà C'
-  },
-  {
-    id: 4,
-    name: 'Phòng 1 người',
-    price: 300000,
-    quantity: 15,
-    capacity: 1,
-    rooms: ['401', '402', '403', '404', '405', '406', '407', '408'],
-    building: 'Tòa nhà A'
-  },
-  {
-    id: 5,
-    name: 'Phòng đôi',
-    price: 550000,
-    quantity: 8,
-    capacity: 2,
-    rooms: ['501', '502', '503', '504'],
-    building: 'Tòa nhà B'
-  }
-]
-
-export default function ManageRoomType() {
-  const [rows, setRows] = useState<GridValidRowModel[]>(initialRows)
-  const [rowModesModel, setRowModesModel] = useState<GridRowModesModel>({})
-  const handleEditClick = (id: GridRowId) => () => {
-    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } })
-  }
-
-  const handleSaveClick = (id: GridRowId) => () => {
-    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } })
-  }
-
-  const handleDeleteClick = (id: GridRowId) => () => {
-    setRows(rows.filter((row) => row.id !== id))
-  }
-
-  const handleCancelClick = (id: GridRowId) => () => {
-    setRowModesModel({
-      ...rowModesModel,
-      [id]: { mode: GridRowModes.View, ignoreModifications: true }
-    })
-
-    const editedRow = rows.find((row) => row.id === id)
-    if (editedRow!.isNew) {
-      setRows(rows.filter((row) => row.id !== id))
+export default function RoomType() {
+  interface RoomType {
+    capacity: number
+    id: number
+    name: string
+    price: number
+    quantity: number
+    building: {
+      address: string
+      status: string
+      id: number
+      description: string
+      hotlineNumber: string
+      createdAt: string
+      updatedAt: string
     }
   }
 
+  const [rows, setRows] = useState<RoomType[]>([])
+  const [paginationModel, setPaginationModel] = useState({ page: 0, pageSize: 10 })
+
+  const { data, isFetching, refetch } = useGetFilterRoomType({
+    page: paginationModel.page + 1,
+    take: paginationModel.pageSize
+  })
+
+  useEffect(() => {
+    if (data?.data.data) {
+      console.log(data.data.data)
+      setRows(data.data.data)
+    }
+  }, [data])
+
+  const totalRowCount = data?.data.totalRecord || 0
+
   const columns: GridColDef[] = [
-    {
-      field: 'id',
-      headerName: 'ID',
-      width: 50,
-      valueFormatter: (params) => {
-        if (params) {
-          return `${params}`
-        } else {
-          return `${rows.length + 1}`
-        }
-      }
-    },
-    {
-      field: 'name',
-      headerName: 'Tên loại phòng',
-      width: 150,
-      editable: true,
-      renderCell: (params: GridRenderCellParams) => <div onClick={handleEditClick(params.id)}>{params.value}</div>
-    },
+    { field: 'id', headerName: 'ID', width: 50 },
+    { field: 'name', headerName: 'Tên', width: 150 },
     {
       field: 'price',
       headerName: 'Giá',
-      width: 150,
-      valueFormatter: (params: { value: number }) => {
-        return `${params.value} đ`
-      },
-      type: 'number',
-      editable: true
+      width: 100,
+      valueGetter: (params: { row: RoomType }) =>
+        params.row?.price ? params.row.price.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' }) : 'N/A'
     },
-    {
-      field: 'quantity',
-      headerName: 'Số lượng',
-      width: 150,
-      type: 'number',
-      editable: true
-    },
-    {
-      field: 'capacity',
-      headerName: 'Sức chứa',
-      width: 150,
-      type: 'number',
-      editable: true
-    },
-    {
-      field: 'rooms',
-      headerName: 'Tên phòng',
-      width: 150,
-      renderCell: (params: GridRenderCellParams) => <div>{params.value.join(', ')}</div>
-    },
+    { field: 'quantity', headerName: 'Số lượng', width: 100 },
+    { field: 'capacity', headerName: 'Sức chứa', width: 100 },
     {
       field: 'building',
       headerName: 'Tòa nhà',
       width: 150,
-      editable: true,
-      renderCell: (params: GridRenderCellParams) => <div>{params.value}</div>,
-      renderEditCell: (params: GridRenderCellParams) => (
-        <Select
-          value={params.value}
-          onChange={(event) => {
-            const newValue = event.target.value
-            console.log(newValue)
-            setRows((oldRows) => oldRows.map((row) => (row.id === params.id ? { ...row, building: newValue } : row)))
-            console.log(rows.filter((row) => row.id === params.id)[0].building)
-          }}
-          fullWidth
-        >
-          {buildings.map((building) => (
-            <MenuItem key={building} value={building}>
-              {building}
-            </MenuItem>
-          ))}
-        </Select>
-      )
+      valueGetter: (params: { row: RoomType }) => params.row?.building?.address || 'N/A'
     },
     {
       field: 'actions',
-      type: 'actions',
       headerName: 'Hành động',
       width: 100,
-      cellClassName: 'actions',
-      getActions: ({ id }) => {
-        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit
-
-        if (isInEditMode) {
-          return [
-            <GridActionsCellItem
-              icon={<SaveIcon />}
-              label='Save'
-              sx={{ color: 'primary.main' }}
-              onClick={handleSaveClick(id)}
-            />,
-            <GridActionsCellItem icon={<CancelIcon />} label='Cancel' onClick={handleCancelClick(id)} color='inherit' />
-          ]
-        }
-
-        return [
-          <GridActionsCellItem icon={<EditIcon />} label='Edit' onClick={handleEditClick(id)} color='inherit' />,
-          <GridActionsCellItem icon={<DeleteIcon />} label='Delete' onClick={handleDeleteClick(id)} color='inherit' />
-        ]
-      }
+      renderCell: (params) => <RoomTypeModal row={params.row} refetch={refetch} action={ACTION.UPDATE} />
     }
   ]
 
   const Toolbar = () => {
-    const handleClick = () => {
-      const id = rows.length + 1
-      const newRows = { id, name: '', price: 0, quantity: 0, capacity: 0, rooms: [], building: '', isNew: true }
-      setRows((oldRows) => [newRows, ...oldRows])
-      setRowModesModel((oldModel) => ({
-        ...oldModel,
-        [id]: { mode: GridRowModes.Edit, fieldToFocus: 'name' }
-      }))
-    }
-
     return (
       <GridToolbarContainer sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Button color='primary' startIcon={<AddIcon />} onClick={handleClick}>
-          Thêm loại phòng
-        </Button>
+        <RoomTypeModal
+          row={{
+            id: 0,
+            name: '',
+            price: 0,
+            quantity: 0,
+            capacity: 0,
+            building: {
+              address: '',
+              status: '',
+              id: 0,
+              description: '',
+              hotlineNumber: '',
+              createdAt: '',
+              updatedAt: ''
+            }
+          }}
+          refetch={refetch}
+          action={ACTION.CREATE}
+        />
+        <GridToolbarQuickFilter />
       </GridToolbarContainer>
     )
   }
@@ -222,17 +99,23 @@ export default function ManageRoomType() {
     <Box sx={{ display: 'flex', flexDirection: 'column', flex: '1 1 auto' }}>
       <Box display='flex' alignItems='center' mb={5}>
         <Typography variant='h4' fontWeight='500' flexGrow={1}>
-          Quản lí loại phòng
+          Quản lí phòng
         </Typography>
       </Box>
-      <Table
-        columns={columns}
-        rows={rows}
-        setRows={setRows}
-        rowModesModel={rowModesModel}
-        setRowModesModel={setRowModesModel}
-        toolbarComponents={Toolbar}
-      />
+
+      {isFetching ? (
+        <CircularProgress />
+      ) : (
+        <Table
+          columns={columns}
+          rows={rows}
+          loading={isFetching}
+          toolbarComponents={Toolbar}
+          paginationModel={paginationModel}
+          setPaginationModel={setPaginationModel}
+          totalRowCount={totalRowCount}
+        />
+      )}
     </Box>
   )
 }
