6c3b8d274c00bcbc28579ba7dc9a40268fc5336b
Huynh Nguyen <yugivip277@gmail.com>
2024-10-18 10:01:33 +0700

    [PBS-68][NguyenHCP] feat: declare api get Filtered building

--- src/apis/building.ts
+++ src/apis/building.ts
@@ -4,6 +4,7 @@ import {
   CreateBuildingBodyType,
   CreateBuildingResType,
   EditBuildingBodyType,
+  GetFilteredBuildingQueryType,
   GetListBuidlingResType
 } from '~/schemaValidations/building.schema'
 import http from '~/utils/http'
@@ -12,6 +13,10 @@ const buildingApiRequest = {
   getListBuidling: (query: Pagination) => {
     const stringified = queryString.stringify(query)
     return http.get<GetListBuidlingResType>(`/buildings?${stringified}`)
+  },
+  getFilteredBuilding: (query: GetFilteredBuildingQueryType) => {
+    const stringified = queryString.stringify(query)
+    return http.get<GetListBuidlingResType>(`/buildings/filtered-building?${stringified}`)
   },
   createBuilding: (body: CreateBuildingBodyType) => http.post<CreateBuildingResType>('/buildings', body),
   editBuilding: (body: EditBuildingBodyType) => http.put<CreateBuildingResType>(`/buildings/${body.id}`, body)
