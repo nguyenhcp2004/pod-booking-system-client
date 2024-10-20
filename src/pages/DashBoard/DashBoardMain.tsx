78b15750c7253d789d3990572728c8a620fe24f3
Huynh Nguyen <yugivip277@gmail.com>
2024-10-20 10:48:18 +0700

    [PBS-65][NguyenHCP] feat: add color to card reports

--- src/pages/DashBoard/DashBoardMain.tsx
+++ src/pages/DashBoard/DashBoardMain.tsx
@@ -27,75 +27,114 @@ export default function DashboardMain() {
       <Grid container spacing={2} marginTop={2}>
         <Grid item xs={12} sm={6} md={3}>
           <Box height='100%'>
-            <Card sx={{ height: '100%' }}>
+            <Card
+              sx={{
+                height: '100%',
+                background: 'linear-gradient(to bottom right, #e3f2fd, #90caf9)',
+                color: '#1e88e5'
+              }}
+            >
               <CardHeader
                 component={Box}
                 title='Tổng doanh thu'
-                sx={{ margin: 0 }}
+                sx={{ margin: 0, color: 'inherit' }}
                 action={
                   <Box>
-                    <AssessmentIcon sx={{ marginTop: 1.2, marginRight: 1 }} />
+                    <AssessmentIcon sx={{ marginTop: 1.2, marginRight: 1, color: 'inherit' }} />
                   </Box>
                 }
               />
               <CardContent>
-                <Typography variant='h5'>0</Typography>
+                <Typography variant='h5' sx={{ color: 'inherit', fontWeight: 'bold' }}>
+                  0
+                </Typography>
               </CardContent>
             </Card>
           </Box>
         </Grid>
+
         <Grid item xs={12} sm={6} md={3}>
           <Box height='100%'>
-            <Card sx={{ height: '100%' }}>
+            <Card
+              sx={{
+                height: '100%',
+                background: 'linear-gradient(to bottom right, #e8f5e9, #a5d6a7)',
+                color: '#43a047'
+              }}
+            >
               <CardHeader
                 title='Khách'
-                sx={{ margin: 0 }}
+                sx={{ margin: 0, color: 'inherit' }}
                 action={
                   <Box>
-                    <PeopleAltIcon sx={{ marginTop: 1.2, marginRight: 1 }} />
+                    <PeopleAltIcon sx={{ marginTop: 1.2, marginRight: 1, color: 'inherit' }} />
                   </Box>
                 }
               />
               <CardContent>
-                <Typography variant='h5'>0</Typography>
-                <Typography variant='caption'>Đặt phòng</Typography>
+                <Typography variant='h5' sx={{ color: 'inherit', fontWeight: 'bold' }}>
+                  0
+                </Typography>
+                <Typography variant='caption' sx={{ color: '#43a047' }}>
+                  Đặt phòng
+                </Typography>
               </CardContent>
             </Card>
           </Box>
         </Grid>
+
         <Grid item xs={12} sm={6} md={3}>
           <Box height='100%'>
-            <Card sx={{ height: '100%' }}>
+            <Card
+              sx={{
+                height: '100%',
+                background: 'linear-gradient(to bottom right, #fff3e0, #ffcc80)',
+                color: '#fb8c00'
+              }}
+            >
               <CardHeader
                 title='Đơn hàng'
-                sx={{ margin: 0 }}
+                sx={{ margin: 0, color: 'inherit' }}
                 action={
                   <Box>
-                    <ShoppingCartIcon sx={{ marginTop: 1.2, marginRight: 1 }} />
+                    <ShoppingCartIcon sx={{ marginTop: 1.2, marginRight: 1, color: 'inherit' }} />
                   </Box>
                 }
               />
               <CardContent>
-                <Typography variant='h5'>0</Typography>
-                <Typography variant='caption'>Đã thanh toán</Typography>
+                <Typography variant='h5' sx={{ color: 'inherit', fontWeight: 'bold' }}>
+                  0
+                </Typography>
+                <Typography variant='caption' sx={{ color: '#fb8c00' }}>
+                  Đã thanh toán
+                </Typography>
               </CardContent>
             </Card>
           </Box>
         </Grid>
+
         <Grid item xs={12} sm={6} md={3}>
           <Box height='100%'>
-            <Card sx={{ height: '100%' }}>
+            <Card
+              sx={{
+                height: '100%',
+                background: 'linear-gradient(to bottom right, #fce4ec, #f48fb1)',
+                color: '#d81b60'
+              }}
+            >
               <CardHeader
                 title='Phòng đang phục vụ'
-                sx={{ margin: 0 }}
+                sx={{ margin: 0, color: 'inherit' }}
                 action={
                   <Box>
-                    <DoorSlidingIcon sx={{ marginTop: 1.2 }} />
+                    <DoorSlidingIcon sx={{ marginTop: 1.2, color: 'inherit' }} />
                   </Box>
                 }
               />
               <CardContent>
-                <Typography variant='h5'>0</Typography>
+                <Typography variant='h5' sx={{ color: 'inherit', fontWeight: 'bold' }}>
+                  0
+                </Typography>
               </CardContent>
             </Card>
           </Box>
