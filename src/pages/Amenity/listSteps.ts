291d7f1e0c1808a57cb560ca4f29279bdb9c78c0
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-10-20 16:39:47 +0700

    [PBS-79][HoangHN] feat: 🦋 base list steps

---
+++ src/pages/Amenity/listSteps.ts
@@ -0,0 +1,21 @@
+import AmenityPage from './AmenityPage'
+
+export const listSteps = [
+  {
+    index: 1,
+    describe: 'Tiện ích',
+    element: AmenityPage
+  },
+  {
+    index: 2,
+    describe: 'Chi tiết'
+  },
+  {
+    index: 3,
+    describe: 'Thanh toán'
+  },
+  {
+    index: 4,
+    describe: 'Kết thúc'
+  }
+]
