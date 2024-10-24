97e78e098c0643c4b83d23d3f390301e9169aca5
Huynh Nguyen <yugivip277@gmail.com>
2024-10-24 20:41:19 +0700

    [NguyenHCP] feat: declare sendMail schema

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -179,3 +179,9 @@ export const formatTime = (dateString: string) => {
   const date = new Date(dateString)
   return date.toLocaleTimeString('vi-VN', { hour: '2-digit', minute: '2-digit', hour12: false })
 }
+
+export const formatDateAndSlot = ({ date, timeSlot }: { date: string; timeSlot: string }) => {
+  const startTime = timeSlot.split(' - ')[0]
+  const dateTime = moment(`${date} ${startTime}`, 'YYYY-MM-DD HH:mm')
+  return dateTime.format('YYYY-MM-DDTHH:mm:ss')
+}
