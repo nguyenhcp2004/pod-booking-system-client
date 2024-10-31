ea87681f0028b21de3993c6761e323e5f111db51
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-11-01 03:51:01 +0700

    [PBS-79][HoangHN] chore: 🐜 disable eslint

--- src/contexts/BookingContext.tsx
+++ src/contexts/BookingContext.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import { createContext, useContext, useState, useEffect } from 'react'
 import { ReactNode } from 'react'
 import { RoomTypeFix, ServicePackage } from '~/constants/type'
