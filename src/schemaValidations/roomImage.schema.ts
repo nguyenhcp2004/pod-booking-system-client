af8b211682e7fdb6436c6429384f38eb4c4fc3f8
HuyDiCode <nbquochuy.forwork@gmail.com>
2024-11-07 01:21:17 +0700

    [PBS-110][HuyNBQ] feat: create response for getImage api

--- src/schemaValidations/roomImage.schema.ts
+++ src/schemaValidations/roomImage.schema.ts
@@ -4,3 +4,16 @@ export const AddImageToRoomBody = z.object({
   image: z.array(z.string())
 })
 export type AddImageToRoomBodyType = z.TypeOf<typeof AddImageToRoomBody>
+
+export const getImagesByRoomIdResponse = z.object({
+  code: z.number(),
+  data: z.array(
+    z.object({
+      id: z.number(),
+      imageUrl: z.string()
+    })
+  ),
+  message: z.string()
+})
+
+export type getImagesByRoomIdResponseType = z.TypeOf<typeof getImagesByRoomIdResponse>
